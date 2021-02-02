0dece23196bf43cbb947e6d41dc9f1511251e896
dhruvkarhe <72870337+dhruvkarhe@users.noreply.github.com>
2021-02-02 13:21:22 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,49 @@
+class Player {
+  constructor() {
+    this.rank = 0;
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+  }
+
+  getCount() {
+    var playerCountRef = database.ref("playerCount");
+    playerCountRef.on("value", (data) => {
+      playerCount = data.val();
+    });
+  }
+
+  updateCount(count) {
+    database.ref("/").update({
+      playerCount: count,
+    });
+  }
+
+  update() {
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name: this.name,
+      distance: this.distance,
+      rank: this.rank,
+    });
+  }
+
+  static getPlayerInfo() {
+    var playerInfoRef = database.ref("players");
+    playerInfoRef.on("value", (data) => {
+      allPlayers = data.val();
+    });
+  }
+  getFinishedplayers() {
+    var ref = database.ref("finishedPlayers");
+    ref.on("value", (data) => {
+      finishedPlayers = data.val();
+    });
+  }
+  static updateFinishedPlayers() {
+    database.ref("/").update({
+      finishedPlayers: finishedPlayers + 1,
+    });
+    this.rank = this.rank + 1;
+  }
+}
